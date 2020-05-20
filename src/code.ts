0e1468894a3e119ef3ad68440d281d7f47174c88
Christian Pajung <christian.pajung@telekom.de>
2020-05-20 16:14:37 +0200

    show next, error and complete in subscription

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,16 @@
 import { Observable } from "rxjs/Observable";
 
 var observable = Observable.create((observer:any) => {
-	observer.next('Hello World!');
+	observer.next('next 1 - will show');
+	observer.next('next 2 - will show');
+	observer.complete();
+	observer.next('next 3 - will NOT show');
 })
 
-observable.subscribe((x:any) => addItem(x));
+observable.subscribe(
+	(x:any) => addItem(x),			// next
+	(error:any) => addItem(error),	// error
+	() => addItem('Completed'));	// completion
 
 function addItem(val:any) {
     var node = document.createElement("li");
