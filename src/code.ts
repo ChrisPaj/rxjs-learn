c9ba4522ee2867ec116b37ca74cb1fe0beb1b60c
Christian Pajung <christian.pajung@telekom.de>
2020-05-20 16:58:15 +0200

    two observers subscribing same observable

--- src/code.ts
+++ src/code.ts
@@ -1,30 +1,34 @@
 import { Observable } from "rxjs/Observable";
 
-var observable = Observable.create((observer:any) => {
-	try {
-		observer.next('next 1 - will show');
-		observer.next('next 2 - will show');
-		setInterval(() => {
-			observer.next('interval');
-		}, 2000);
-	} catch (err) {
-		observer.error(err)
-	}
-	
-})
+var observable = Observable.create((observer: any) => {
+  try {
+    observer.next("next 1 - will show");
+    observer.next("next 2 - will show");
+    setInterval(() => {
+      observer.next("interval");
+    }, 2000);
+  } catch (err) {
+    observer.error(err);
+  }
+});
 
 var observer = observable.subscribe(
-	(x:any) => addItem(x),			// next
-	(error:any) => addItem(error),	// error
-	() => addItem('Completed'));	// completion
+  (x: any) => addItem(x), // next
+  (error: any) => addItem(error), // error
+  () => addItem("Completed") // completion
+); 
+
+var observer2 = observable.subscribe(
+  (x: any) => addItem(x + ' observer2'), 
+)
 
 setTimeout(() => {
-	observer.unsubscribe();
-}, 7000)
+  observer.unsubscribe();
+}, 7000);
 
-function addItem(val:any) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(val);
-    node.appendChild(textnode);
-    document.getElementById("output").appendChild(node);
+function addItem(val: any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
 }
