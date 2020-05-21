7ef6efd4161f1182c384b02c9587b9dfa2c0b246
Christian Pajung <christian.pajung@telekom.de>
2020-05-21 08:20:09 +0200

    ReplaySubject2

--- src/code.ts
+++ src/code.ts
@@ -1,12 +1,12 @@
 import { ReplaySubject } from "rxjs/ReplaySubject";
 
-var subject = new ReplaySubject(5);
+var subject = new ReplaySubject(5, 150); // (num, ms)
 
 var i = 1;
 var int = setInterval(() => subject.next(i++), 100);
 
 setTimeout(() => {
-	// last 5 values are shown
+	// last 5 values are shown created within the last 150ms
 	subject.subscribe(
 		data => addItem('Observer 1: '+ data),
 		err => addItem(err),
