4669e949fa8c5a564721170142058c29b0a8915e
Christian Pajung <christian.pajung@telekom.de>
2020-05-21 08:48:56 +0200

    AsyncSubject2

--- src/code.ts
+++ src/code.ts
@@ -1,12 +1,13 @@
 import { AsyncSubject } from "rxjs/AsyncSubject";
 
-// only last data is shown
+// only last data is shown, .complete() must be called
 var subject = new AsyncSubject();
 
 var observer1 = subject.subscribe(
 	data => addItem('Observer 1: '+ data),
 	err => addItem(err),
-	() => addItem('Observer 1 Completed') // shown, too
+	// shown, because subject.complete() must be called
+	() => addItem('Observer 1 Completed') 
 )
 
 var i = 1;
@@ -16,9 +17,10 @@ setTimeout(() => {
 	var observer2 = subject.subscribe(
 		data => addItem('Observer 2: '+ data),
 		err => addItem(err),
-		() => addItem('Observer 2 Completed') // shown, too
+		// shown, because subject.complete() must be called
+		() => addItem('Observer 2 Completed') 
 	)
-	subject.complete();
+	subject.complete(); // must be called
     clearInterval(int);
 }, 800);
 
