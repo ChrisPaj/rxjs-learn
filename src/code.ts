6202546742208ff77993bf9328b718188c2c7385
Christian Pajung <christian.pajung@telekom.de>
2020-05-23 09:04:59 +0200

    skipUntil operator

--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,26 @@
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map";
+import { Subject } from "rxjs/Subject";
+import "rxjs/add/operator/skipUntil";
 
-    Observable.create((observer:any) => {
-        observer.next('Hey guys!')
-    })
-    .map((val:any) => val.toUpperCase())
-    .subscribe((x:any) => addItem(x));
+var observable1 = Observable.create((data:any) => {
+    var i = 1
+    setInterval(() => {
+        data.next(i++)
+    }, 1000)
+})
+
+var observable2 = new Subject();
+
+setTimeout(() => {
+    observable2.next('Hey!')
+}, 5000)
+
+
+// Returns an Observable that skips items emitted by the 
+// source Observable until a second Observable emits an item.
+var newObs = observable1.skipUntil(observable2)
+
+newObs.subscribe((x:any) => addItem(x));
 
 function addItem(val: any) {
   var node = document.createElement("li");
